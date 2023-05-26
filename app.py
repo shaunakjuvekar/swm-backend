d1303328cb8227234e767685e3b3e85ed4caa88d
Shaunak Juvekar <shaunakjuvekar@Shaunaks-Air.hsd1.tx.comcast.net>
2023-05-26 12:11:47 -0500

    updated files

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import json
+#import csv_convert as csv_transform
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -25,8 +27,20 @@ def receive_data():
     #title = request.json['title']
     #body = request.json['body']
     locations = request.json
+    #csv_transform.convert_csv(locations)
     print(locations)
     return {'success':'1'}
+
+
+@app.route("/get_data", methods=["GET"], strict_slashes=False)
+@cross_origin(support_credentials=True)
+def send_data():
+    #title = request.json['title']
+    #body = request.json['body']
+    with open("data_file.json", "r") as data:
+        route_data = json.load(data)
+    print(route_data)
+    return route_data
 
 if __name__=='__main__':
     app.run(debug=True, port=5002)
--- csv_convert.py
+++ csv_convert.py
@@ -1,14 +1,23 @@
 import csv
 import ast
+import json
 
+frontend_data = []
+
+#def convert_csv():
 with open('sample_output.csv', 'r', newline='') as inputFile, open('output_final.csv', 'w', newline='') as writerFile:
     read_file = csv.reader(inputFile)
     write_file = csv.writer(writerFile, delimiter=',')
     for row in read_file:
+        obj = {}
         lat, long, id, cost_demand, node_label = row[0], row[1], row[2], row[3], row[4]
         echelon, index = row[5], row[6]
         routes, route_costs = row[7], row[8]
         capacity, capacity_costs = row[9], row[10]
+        obj['lat'], obj['lng'], obj['label'], obj['echelon'] =lat,long, node_label, echelon
+        obj['routes'], obj['route_costs'] = routes, route_costs 
+        
+        frontend_data.append(obj)
         if routes and routes!='routes':
             write_file.writerow(['Facility', ' Echelon'])
             write_file.writerow([node_label, echelon])
@@ -16,10 +25,14 @@ with open('sample_output.csv', 'r', newline='') as inputFile, open('output_final
             write_file.writerow(['Capacity', capacity, capacity_costs])
             write_file.writerow(['Location', node_label, cost_demand])
             write_file.writerow(['Vehicle', 'Node Label(s) served in echelon '+str(int(echelon)-1), 'Cost'])
-          
+        
             routes_arr = (ast.literal_eval(routes))
             costs_arr = (ast.literal_eval(route_costs))
             for i in range(len(routes_arr)):
                 write_file.writerow([i+1, routes_arr[i], costs_arr[i]])
             
             write_file.writerow([])
+    #print(frontend_data)
+
+with open("data_file.json", "w") as wf:
+    json.dump(frontend_data[1:], wf)
